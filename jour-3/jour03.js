286260127d0d0a08e38d67b2b0c6583456a347bb
Youbess <alexexocet@gmail.com>
2022-01-27 17:11:53 +0100

    first commit

---
+++ jour-3/jour03.js
@@ -0,0 +1,37 @@
+// 01 - array
+
+//let fruits = ['mango','lemon','blueberry'];
+//console.log(fruits);
+
+// 02 - access
+
+//let ingredients = ['eggs','milk','butter'];
+//console.log(ingredients[1]);
+
+// 03 - add and remove
+
+//let objets = ['pen','book','lamp'];
+//objets.push('chair');
+//console.log(objets);
+//objets.splice(2,1);
+//console.log(objets);
+//objets.push('laptop');
+//console.log(objets);
+//objets.splice(2,1);
+//console.log(objets);
+
+// 04 - order
+
+//let numbers = [4,10,8,12,6];
+//numbers.reverse();
+//console.log(numbers);
+//numbers.sort((a,b) => a- b);
+//console.log(numbers);
+
+// 05 - boucle
+
+//let total = 0;
+//let limit = 10;
+//for (let i = 0; i <=limit; i++) {
+//    console.log(i);
+//}
---
+++ jour-4/jour04.js
@@ -0,0 +1,9 @@
+// 01 - object
+
+let cat = {
+    name:'garfield',
+    age: '3',
+    isCute: true,
+};
+console.log(cat);
+console.log(cat['age']);
