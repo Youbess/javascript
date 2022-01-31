04fad54b3d12b7105c6111d37fbfbadf2ecbf272
Youbess <alexexocet@gmail.com>
2022-01-31 15:29:48 +0100

    first commit

--- jour-4/jour04.js
+++ jour-4/jour04.js
@@ -2,8 +2,29 @@
 
 let cat = {
     name:'garfield',
-    age: '3',
+    age: 3,
     isCute: true,
 };
 console.log(cat);
 console.log(cat['age']);
+if (cat.isCute) {
+    console.log('so cute !')
+}
+
+// 02 - combine
+
+let cat2 = {
+    name:'exel',
+    age:2,
+    isCute: true,
+};
+
+let cats = (cat,cat2);
+
+console.log
+
+// 03 - even 
+
+function checkIfEven(num) {
+
+}
