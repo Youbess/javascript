3325bc371baceeb080a6535f3946f7bdadefd2d6
Youbess <alexexocet@gmail.com>
2022-02-01 17:31:06 +0100

    first commit

---
+++ les boucles/forLoop.js
@@ -0,0 +1,35 @@
+// 01 - somme des carrés
+
+let total = 0;
+for (let i = 5; i <= 10;i++) {
+    let carre = i*i;
+    total = total+carre;
+}
+
+console.log(total);
+
+// 02 - comptons
+
+let tout = 0
+for (let i=100; i <= 1000;i++) {
+  if (i % 7 == 0) {
+      tout = tout +1
+  }
+}
+
+console.log(tout);
+
+// 03 -chanceux
+
+let min = 1;
+let max = 6;
+let lancer = 20;
+
+
+for (let i= 1; i <=20; i++) {
+    let floor = Math.floor(Math.random()*(max-min+1)+min);
+    if ( floor >=5) {
+        console.log(floor)
+    }
+}
+
