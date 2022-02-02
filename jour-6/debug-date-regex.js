e8ad98617dd1f70139c265636f3ed2d81b427a26
Youbess <alexexocet@gmail.com>
2022-02-02 15:58:59 +0100

    first commit

---
+++ jour-6/debug-date-regex.js
@@ -0,0 +1,23 @@
+// 01 - format
+
+// function formatDate(date) {
+    // console.log(date)
+    // let chiffre = date.split('-');
+    // chiffre.reverse();
+    // console.log(chiffre);
+    // let newDate = chiffre.join('/');
+    // console.log(newDate);
+    // return newDate
+// }
+// let formatedDate = formatDate('2022-02-22')
+
+// console.log(formatedDate);
+
+// 02 - age
+function calculateAge(date){
+let birthday = new Date('1983-05-24');
+console.log(birthday);
+
+}
+
+let calculateAge = calculateAge('2022-02-22');
