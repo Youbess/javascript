5244678e7630cfe41eb739938428ac062fdc1a66
Youbess <alexexocet@gmail.com>
2022-02-07 11:06:51 +0100

    first commit

---
+++ jour-7/jour07.js
@@ -0,0 +1,69 @@
+// 01 fyle system
+
+// const fs = require('fs');
+
+// fs.readFile('jour07.txt','utf-8',(err,data) => {
+
+	// if(err) {
+		// console.error(err)
+		// return
+	// }
+	// const fileContent = data.toString()
+
+	// console.log('readFile data>>>',data);
+// }); 
+
+// 02 - Map double
+
+// let array = [1,2,3,4,5];
+// let double = array.map((num) => num*2);
+// console.log(double);
+
+// 03 - Map names
+
+// let longNames = [
+	// {
+		// firstName: "Jane",
+		// lastName: "Doe"
+	// },
+	// {
+		// firstName: "John",
+		// lastName: "Smith"
+	// }
+// ]
+
+// let shortNames = longNames.map(function(string) {
+
+    // let firstAndLast = { name: string.firstName +' '+string.lastName }
+    
+    // return firstAndLast 
+// });
+
+//  console.log(shortNames);
+
+// 04 - Filter Numbers
+
+// let array = [ 1, 'toto' , 34 , 'javascript' , 8 ];
+
+// let numbers = array.filter((element) => {
+
+
+    // return typeof element ==='number';
+
+// });
+
+// console.log(numbers);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
